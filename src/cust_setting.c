b64544110dfec106f20e50626c480b403f0b44fd
lisiqi <alienwalker@sina.com>
2017-08-07 08:29:02 +0800

    门限电压调整

--- src/cust_setting.c
+++ src/cust_setting.c
@@ -200,7 +200,7 @@ void Param_Config(void)
 		Param->Data.ParamDW.Param[PARAM_COM_BR] = HAL_UART_BAUD_RATE_9600;
 		Param->Data.ParamDW.Param[PARAM_GPS_BR] = HAL_UART_BAUD_RATE_9600;
 		Param->Data.ParamDW.Param[PARAM_STOP_VBAT] = 3400;
-		Param->Data.ParamDW.Param[PARAM_LOW_VBAT] = 3700;
+		Param->Data.ParamDW.Param[PARAM_LOW_VBAT] = 3600;
 		Param->Data.ParamDW.Param[PARAM_NORMAL_VBAT] = 3900;
 		Param->Data.ParamDW.Param[PARAM_SMS_ALARM] = 0;
 		Param->Data.ParamDW.Param[PARAM_CALL_AUTO_GET] = 0;
